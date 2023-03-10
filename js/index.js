41d049653249bc63cd8755e91b6fc5cac16b2577
jnsgdm <JonasDmoreira@hotmail.com>
2023-03-10 12:18:08 -0300

    .

--- js/index.js
+++ js/index.js
@@ -1,15 +1,8 @@
-// const stars = document.querySelector("#stars")
-// window.addEventListener('scroll',()=>{
-//     let value = window.screenY;
-//     stars.style.top = value * 1 + "px";
-// });
-
 var checkNav = false
 const navLinks = document.querySelector('#nav-links');
 function handleNavMobile(value) {
     checkNav = !checkNav;
     if(checkNav){
-        navLinks.style.position = 'fixed';
         navLinks.style.left = '0';
     }else {
         navLinks.style.left = '-100%';
