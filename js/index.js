f9738d4335ccb0ec95d7933a9725be6e2451a714
jnsgdm <JonasDmoreira@hotmail.com>
2023-03-09 22:52:01 -0300

    .

--- js/index.js
+++ js/index.js
@@ -5,18 +5,34 @@
 // });
 
 var checkNav = false
+const navLinks = document.querySelector('#nav-links');
 function handleNavMobile(value) {
-    const navLinks = document.querySelector('#nav-links');
     checkNav = !checkNav;
     if(checkNav){
+        navLinks.style.position = 'fixed';
         navLinks.style.left = '0';
     }else {
         navLinks.style.left = '-100%';
     }
-}
+};
 
-function handleHideNav(){
-    const navLinks = document.querySelector('#nav-links');
+const aboutLink = document.querySelector('#about-link');
+aboutLink.addEventListener('click',() => {
     navLinks.style.left = '-100%';
     console.log("teste cu");
-}
+});
+const projecttLink = document.querySelector('#project-link');
+projecttLink.addEventListener('click',() => {
+    navLinks.style.left = '-100%';
+    console.log("teste cu");
+});
+const skillLink = document.querySelector('#skill-link');
+skillLink.addEventListener('click',() => {
+    navLinks.style.left = '-100%';
+    console.log("teste cu");
+});
+const contactLink = document.querySelector('#contact-link');
+contactLink.addEventListener('click',() => {
+    navLinks.style.left = '-100%';
+    console.log("teste cu");
+});
