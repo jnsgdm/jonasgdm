d5c999952050e6451666a5aafe2973ba1c9edaa7
jnsgdm <JonasDmoreira@hotmail.com>
2023-03-17 13:15:04 -0300

    .

--- js/index.js
+++ js/index.js
@@ -55,3 +55,13 @@ contactLink.addEventListener('click',() => {
     console.log("teste cu");
 });
 
+// function lavaFunc(){
+//     window.lavaVar += 90;
+//     document.getElementsByClassName("lavaLamp")[0].style.filter = "hue-rotate("+window.lavaVar+"deg)";
+//     document.getElementsByClassName("lavaLamp")[0].style.animationName = "none";
+//     setTimeout(function(){
+//       document.getElementsByClassName("lavaLamp")[0].style.animationName = "";
+//       document.getElementsByClassName("lavaLamp")[0].style.animationPlayState = "running";
+//     }, 50);
+//   }
+//   window.lavaVar = 0;
